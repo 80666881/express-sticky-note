3dc83cdcfc51739212d65f4d9bcb0fa2a726081b
zeller <87192329@163.com>
2017-09-15 00:11:15 +0800

    fix database

--- routes/auth.js
+++ routes/auth.js
@@ -20,8 +20,8 @@ passport.deserializeUser(function (obj, done) {
 
 passport.use(new GitHubStrategy({
     //阿里服务器设置
-    clientID: '4ed0100edb341c96d104',
-    clientSecret: '4c4c444a2758c9f9eeeb088b9071412d14bc714d',
+    clientID: 'f73b4d8e041c12f52afd',
+    clientSecret: '1e67ed8848c988512fa42103142ab43c734ebc58',
     callbackURL: "http://www.zhengzehao.top/auth/github/callback"
 
     //本地localhost设置
